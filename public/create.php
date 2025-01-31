e8b8e75c4b223745e501cf183856569e90c46b67
Paul-Vismonte <paulvismonte0@gmail.com>
2025-01-31 09:32:17 +0800

    "update"

--- public/create.php
+++ public/create.php
@@ -13,5 +13,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <form method="POST">
     <input type="text" name="name" placeholder="Name" required>
     <input type="email" name="email" placeholder="Email" required>
-    <button type="submit">Create</button>
+    <button type="submit">Create now</button>
 </form>
