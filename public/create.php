bb406a6fd85c11b151004ac96ebafc08790ac8f8
Paul-Vismonte <paulvismonte0@gmail.com>
2025-01-31 09:26:21 +0800

    "index,create, and crud"

---
+++ public/create.php
@@ -0,0 +1,17 @@
+<?php
+include '../functions/crud.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (create($_POST['name'], $_POST['email'])) {
+        header("Location: index.php");
+    } else {
+        echo "Failed to create user.";
+    }
+}
+?>
+
+<form method="POST">
+    <input type="text" name="name" placeholder="Name" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <button type="submit">Create</button>
+</form>
---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+include '../functions/crud.php';
+$users = read();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Users List</title>
+    <link rel="stylesheet"href="../css/style.css">
+</head>
+<body>
+    <h2>User List</h2>
+    <a href="create.php">Add New User</a>
+    <table border="1">
+        <tr><th>ID</th><th>Name</th><th>Email</th><th>Actions</th></tr>
+        <?php foreach ($users as $user): ?>
+            <tr>
+                <td><?= $user['id'] ?></td>
+                <td><?= $user['name'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td>
+                    <a href="update.php?id=<?= $user['id'] ?>">Edit</a> |
+                    <a href="delete.php?id=<?= $user['id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
