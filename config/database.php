8db7eac194363231ce0739951503ab4499a33396
Paul-Vismonte <paulvismonte0@gmail.com>
2025-01-31 08:37:55 +0800

    "structure"

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<<?php 
+$host = "localhost";
+$db_name = "crud_db";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+if($conn->connect_error){
+    die("Connection failed: " .
+$conn->connect_error);
+}
+
+
+
+?>
